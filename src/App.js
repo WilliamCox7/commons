c25d3d51dc858a457a5f26583746a334e1b2354d
William Cox <willubyu7@gmail.com>
2017-04-14 21:17:29 -0600

    added login view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="home">
+        <div className="home-logo">
+          <img src={logo} />
+          <h1>commons</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="auth">
+          <button>log in with facebook</button>
+          <p>or</p>
+          
+        </div>
       </div>
     );
   }
