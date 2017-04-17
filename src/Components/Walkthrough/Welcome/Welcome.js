a87d708312f4984617a1b8c5963dc58fdfcef98c
William Cox <willubyu7@gmail.com>
2017-04-17 13:24:39 -0600

    sized down welcome image

--- src/Components/Walkthrough/Welcome/Welcome.js
+++ src/Components/Walkthrough/Welcome/Welcome.js
@@ -6,10 +6,9 @@ class Welcome extends Component {
     return (
       <div className="welcome">
         <div className="above-welcome">
-          <h1>WELCOME</h1>
+          <h1>W E L C O M E</h1>
           <a href="/#/hobbies"><button>let's get started</button></a>
         </div>
-        <div className="faded-orange"></div>
       </div>
     );
   }
